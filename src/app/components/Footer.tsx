dd4223d822cd841164053b8687cf0fd727b400ba
eman <emansajjad863@gmail.com>
2026-04-21 04:14:42 -0700

    correct footer space

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link"
 export default function Footer(){
     return(
-        <div className="bg-gray-100 py-10 mt-20 border-t border-gray-400">
-            <div className=" max-w-6xl mx-auto flex fel-col md:flex-row justify-between items-center gap-8 px-6">
-                <div className="text-2xl font-bold text-purple-600">VibeFlow</div>
-                <div className="flex flex-wrap justify-center items-center gap-x-6 gap-y-4 text-sm font-medium text-purple-600 text-center">
+        <div className="bg-gray-100 py-6 mt-20 border-t border-gray-400">
+            <div className=" max-w-6xl mx-auto flex flex-row justify-between items-center px-4">
+                <div className="text-lg md:text-xl  font-bold text-purple-600">VibeFlow</div>
+                <div className="flex flex-row justify-center items-center gap-3 md:gap-8 text-[11px] md:text-sm font-medium text-purple-600 ">
                     <Link href='/' className="hover:text-gray-600 transition-colors">Home</Link>
                     <Link href='/trend' className="hover:text-gray-600 transition-colors">Trend</Link>
                     <Link href='/illustation' className="hover:text-gray-600 transition-colors">Illustration</Link>
